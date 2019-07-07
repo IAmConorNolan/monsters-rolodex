fe06df6a998f11c7f628cbd8e9a9a36acdd6f341
IAmConorNolan <nolanc14@tcd.ie>
2019-07-07 02:21:50 +0100

    Adding files for GitHub pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
-function App() {
-  return (
+class App extends React.Component { //Using a class gives us access to the state property, also gives set state.
+constructor() {
+  super();
+  this.state = {
+    monsters: [],
+    searchField: '',
+  };
+}
+
+componentDidMount () {
+  //This code gets called when writing to the DOM for the first time
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json()) //Parses Json
+  .then(users=> this.setState({monsters: users})); //Stores users in this.state.monsters (Overwrites)
+}
+
+handleChange = (e) => { //arrow functions automatically bind 'this' to where it was defined
+  this.setState({ searchField: e.target.value });
+};
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(el => 
+      el.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox 
+      placeholder='search monsters'
+      handleChange={this.handleChange}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
