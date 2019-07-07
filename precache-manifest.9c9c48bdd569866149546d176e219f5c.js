fc5fe93eb1e592f19cad2cca0660e9a99707bc5c
IAmConorNolan <nolanc14@tcd.ie>
2019-07-07 02:21:21 +0100

    Updates

---
+++ precache-manifest.9c9c48bdd569866149546d176e219f5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14693d2b29bd739eb71b96f6621322ea",
+    "url": "/IAmConorNolan/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4b180f8422f8691a806f",
+    "url": "/IAmConorNolan/monsters-rolodex/static/css/main.38f87869.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/IAmConorNolan/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4b180f8422f8691a806f",
+    "url": "/IAmConorNolan/monsters-rolodex/static/js/main.8d1b4c07.chunk.js"
+  },
+  {
+    "revision": "7ae61863def1b61888b7",
+    "url": "/IAmConorNolan/monsters-rolodex/static/js/runtime~main.6c2b9e99.js"
+  }
+]);
